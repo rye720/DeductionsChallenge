b8df879c7f886e2404bd221fab1a03dcc1e498af
R Compton <RCompton@focustapps.com>
2021-09-15 02:28:23 -0400

    update script file

--- DeductionsCalculator/DeductionsCalculator/SQLScripts/scripts.sql
+++ DeductionsCalculator/DeductionsCalculator/SQLScripts/scripts.sql
@@ -1,11 +1,13 @@
 --scripts
 -------------------
---CREATE DATABASE ChallengeDatabase
+--CREATE DATABASE [CodeChallenge]
 
 --SELECT Name from sys.Databases
 
-USE ChallengeDatabase
+USE [CodeChallenge]
+GO
 
+------------------------------------------------------------------------------------------------------------
 SET ANSI_NULLS ON
 GO
 
@@ -13,11 +15,11 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Employee](
-	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
-	[IsActive] [bit] NOT NULL DEFAULT 1,
-	[CreatedAt] [datetimeoffset] NOT NULL DEFAULT SYSDATETIMEOFFSET(),
-	[UpdatedAt] [datetimeoffset] NOT NULL DEFAULT SYSDATETIMEOFFSET()
+	[IsActive] [bit] NOT NULL,
+	[CreatedAt] [datetimeoffset](7) NOT NULL,
+	[UpdatedAt] [datetimeoffset](7) NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -25,14 +27,28 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+ALTER TABLE [dbo].[Employee] ADD  DEFAULT (newid()) FOR [Id]
+GO
+
+ALTER TABLE [dbo].[Employee] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+
+ALTER TABLE [dbo].[Employee] ADD  DEFAULT (sysdatetimeoffset()) FOR [CreatedAt]
+GO
+
+ALTER TABLE [dbo].[Employee] ADD  DEFAULT (sysdatetimeoffset()) FOR [UpdatedAt]
+GO
+
+
+------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE [dbo].[Dependent](
-	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
 	[EmployeeId] [uniqueidentifier] NOT NULL,
-	[IsActive] [bit] NOT NULL DEFAULT 1,
-	[CreatedAt] [datetimeoffset] NOT NULL DEFAULT SYSDATETIMEOFFSET(),
-	[UpdatedAt] [datetimeoffset] NOT NULL DEFAULT SYSDATETIMEOFFSET()
+	[IsActive] [bit] NOT NULL,
+	[CreatedAt] [datetimeoffset](7) NOT NULL,
+	[UpdatedAt] [datetimeoffset](7) NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -40,28 +56,59 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+ALTER TABLE [dbo].[Dependent] ADD  DEFAULT (newid()) FOR [Id]
+GO
+
+ALTER TABLE [dbo].[Dependent] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+
+ALTER TABLE [dbo].[Dependent] ADD  DEFAULT (sysdatetimeoffset()) FOR [CreatedAt]
+GO
+
+ALTER TABLE [dbo].[Dependent] ADD  DEFAULT (sysdatetimeoffset()) FOR [UpdatedAt]
+GO
 
 ALTER TABLE [dbo].[Dependent]  WITH CHECK ADD  CONSTRAINT [FK_Dependent_Employee] FOREIGN KEY([EmployeeId])
 REFERENCES [dbo].[Employee] ([Id])
+GO
 
+ALTER TABLE [dbo].[Dependent] CHECK CONSTRAINT [FK_Dependent_Employee]
+GO
+
+
+
+---------------------------------------------------------------------------------------
 
 CREATE TABLE [dbo].[BenefitsPackage](
-	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+	[Id] [uniqueidentifier] NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
-	[YearlyEmployeeCost] [decimal](19,4) NOT NULL,
-	[YearlyDependentCost] [decimal](19,4) NOT NULL,
+	[YearlyEmployeeCost] [decimal](19, 4) NOT NULL,
+	[YearlyDependentCost] [decimal](19, 4) NOT NULL,
 	[DiscountInitial] [nvarchar](1) NULL,
-	[DiscountInitialPercentage] [decimal](5,4) NULL,
+	[DiscountInitialPercentage] [decimal](5, 4) NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreatedAt] [datetimeoffset](7) NOT NULL,
+	[UpdatedAt] [datetimeoffset](7) NOT NULL,
 	[IsDefault] [bit] NULL,
-	[IsActive] [bit] NOT NULL DEFAULT 1,
-	[CreatedAt] [datetimeoffset] NOT NULL DEFAULT SYSDATETIMEOFFSET(),
-	[UpdatedAt] [datetimeoffset] NOT NULL DEFAULT SYSDATETIMEOFFSET()
 PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+ALTER TABLE [dbo].[BenefitsPackage] ADD  DEFAULT (newid()) FOR [Id]
+GO
+
+ALTER TABLE [dbo].[BenefitsPackage] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+
+ALTER TABLE [dbo].[BenefitsPackage] ADD  DEFAULT (sysdatetimeoffset()) FOR [CreatedAt]
+GO
+
+ALTER TABLE [dbo].[BenefitsPackage] ADD  DEFAULT (sysdatetimeoffset()) FOR [UpdatedAt]
+GO
+
 
 
 --insert data given from challenge prompt
